d39d88917467430737174186be4ee0538bdf8d19
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2024-02-04 22:52:12 +0100

    UPDATE

--- main.js
+++ main.js
@@ -1,42 +1,332 @@
 
+
+const translations = {
+    eng: {
+        name : "Bouda Mohamed",
+        header: ['Services', 'Projects', 'About me', 'My Skills','English'],
+        landing: ['Heey', 'My Name Is', 'Bouda Mohamed'],
+        Services: {
+            title: "Services",
+            desc: "I'm a Full Stack Developer with expertise in HTML, CSS, JavaScript, and React for the front-end, and PHP with Laravel and Codeigniter4 for the back-end. I can build complete web applications, ensuring excellent user interfaces and strong server-side functionality. My aim is to provide efficient and seamless software solutions tailored to your project requirements",
+            qualite: [
+                {
+                    title: "CRAFTING DIGITAL EXPERIENCES",
+                    desc: "I possess a talent for expertly crafting memorable and user-centric digital experiences.",
+                },
+                {
+                    title: "TECHNICAL SKILLS",
+                    desc: "I'm a Full Stack Developer with expertise in JavaScript, React, PHP frameworks, and databases. I excel at creating web applications by seamlessly integrating front-end and back-end technologies.",
+                },
+                {
+                    title: "PROBLEM SOLVER BY NATURE",
+                    desc: "I have a natural inclination for problem-solving, and I find genuine satisfaction in discovering effective solutions.",
+                },
+            ]
+        },
+        Aboutme: {
+            title: 'ABOUT ME',
+            question: 'who is Bouda Mohamed?',
+            desc: `Full Stack Developer Hello back, I'm a web Developer and I'm fascinated by the intersection of design and functionality on the web. I enjoy creating user-friendly and visually appealing websites, with a keen eye for detail and a love for problem-solving, I'm constantly honing my skills to stay up-to-date with the latest web technologies. When I'm not coding, I like exploring new design trends, and experimenting with personal projects.`,
+        },
+        project : ['All','WEB','APP WEB','APP MOBILE','WEB DESKTOP'],
+        
+        myskills: ['web technology', 'programming language.'],
+
+        footer : ['Bouda Mohamed','All rights reserved']
+    },
+    fr: {
+        name : "Bouda Mohamed",
+        header: ['Services', 'Projets', 'À propos de moi', 'Mes compétences','Francais'],
+        landing: ['Heey', 'Je m\'appelle', 'Bouda Mohamed'],
+        Services: {
+            title: "Services",
+            desc: "Je suis un développeur Full Stack avec une expertise en HTML, CSS, JavaScript et React pour le front-end, et PHP avec Laravel et Codeigniter4 pour le back-end. Je peux construire des applications web complètes, garantissant d'excellentes interfaces utilisateur et une fonctionnalité robuste côté serveur. Mon objectif est de fournir des solutions logicielles efficaces et sans faille adaptées à vos besoins de projet.",
+            qualite: [
+                {
+                    title: "CONCEPTION D'EXPÉRIENCES NUMÉRIQUES",
+                    desc: "Je possède un talent pour créer des expériences numériques mémorables et centrées sur l'utilisateur.",
+                },
+                {
+                    title: "COMPÉTENCES TECHNIQUES",
+                    desc: "Je suis un développeur Full Stack avec une expertise en JavaScript, React, frameworks PHP et bases de données. J'excellente dans la création d'applications web en intégrant de manière transparente les technologies front-end et back-end.",
+                },
+                {
+                    title: "RÉSOLVEUR DE PROBLÈMES PAR NATURE",
+                    desc: "J'ai une inclination naturelle pour la résolution de problèmes et je trouve une satisfaction réelle à découvrir des solutions efficaces.",
+                },
+            ]
+        },
+        project : ['All','WEB','APP WEB','APP MOBILE','WEB DESKTOP'],
+        Aboutme: {
+            title: 'À PROPOS DE MOI',
+            question: 'qui est Bouda Mohamed ?',
+            desc: `Développeur Full Stack Bonjour à tous, je suis un développeur web et je suis fasciné par l'intersection du design et de la fonctionnalité sur le web. J'aime créer des sites web conviviaux et esthétiquement plaisants, avec un œil attentif aux détails et un amour pour la résolution de problèmes. Je perfectionne constamment mes compétences pour rester à jour avec les dernières technologies web. Quand je ne code pas, j'aime explorer de nouvelles tendances en matière de design et expérimenter avec des projets personnels.`,
+        },
+        myskills: ['technologie web', 'langage de programmation.'],
+        footer: ['Bouda Mohamed', 'Tous droits réservés'],
+    },
+    ar: {
+        
+        header: ["بودة محمد",'الخدمات', 'المشاريع', 'حولي', 'مهاراتي','العربية'],
+        landing: ['مرحبًا', 'اسمي', 'بودا محمد'],
+        Services: {
+            title: "الخدمات",
+            desc: "أنا مطور Full Stack بخبرة في HTML و CSS و JavaScript و React للواجهة الأمامية ، و PHP مع Laravel و Codeigniter4 للواجهة الخلفية. يمكنني بناء تطبيقات ويب كاملة ، مضمنًا واجهات مستخدم ممتازة ووظائف قوية من الجانب الخادم. هدفي هو توفير حلاً برمجيًا فعالًا وسلسًا يتناسب مع متطلبات مشروعك.",
+            qualite: [
+                {
+                    title: "صياغة تجارب رقمية",
+                    desc: "أمتلك موهبة صياغة تجارب رقمية لا تُنسى ومتمحورة حول المستخدم.",
+                },
+                {
+                    title: "مهارات تقنية",
+                    desc: "أنا مطور Full Stack بخبرة في JavaScript و React و PHP frameworks وقواعد البيانات. أبرع في إنشاء تطبيقات الويب من خلال دمج تقنيات الواجهة الأمامية والخلفية بسلاسة.",
+                },
+                {
+                    title: "حل المشكلات بطبيعتي",
+                    desc: "لدي ميل طبيعي لحل المشكلات ، وأجد رضى حقيقيًا في اكتشاف حلول فعّالة.",
+                },
+            ]
+        },
+        project : ['الكل','مواقع','تطبيقات الويب','تطبيقات الهاتف','تطبيقات الحاسوب'],
+        Aboutme: {
+            title: 'عني',
+            question: 'من هو بودا محمد؟',
+            desc: `مطور Full Stack مرحبًا بالجميع ، أنا مطور ويب وأنا مفتون بتقاطع التصميم والوظيفة على الويب. أستمتع بإنشاء مواقع ويب سهلة الاستخدام وجذابة بصريًا ، مع عين حادة للتفاصيل وحب لحل المشكلات ، أنا دائمًا أقوم بتحسين مهاراتي للبقاء على اطلاع بأحدث تقنيات الويب. عندما لا أقوم بالبرمجة ، أستمتع باستكشاف اتجاهات التصميم الجديدة وتجربة المشاريع الشخصية.`,
+        },
+        myskills: ['تقنية الويب', 'لغة البرمجة.'],
+        footer: ['بودا محمد', 'كل الحقوق محفوظة']
+    },
+};
+
+var lang = '';
+
+if(localStorage.getItem('lang')){
+    lang = localStorage.getItem('lang');
+    handleLang(localStorage.getItem('lang'));
+
+}
+else{
+    lang = 'eng';
+    translate('eng');
+}
+
+
+
+
+
+
+
 const projects = [
     {
         "name": "GENIE LOG",
-        "desc": "A complete website with front-end and back-end aspects. The site showcases the services of Genie Log and also includes an administration section to manage clients and their requests.",
+        "desc": {
+            eng: {
+                desc: "A complete website with front-end and back-end aspects. The site showcases the services of Genie Log and also includes an administration section to manage clients and their requests.",
+            },
+            fr: {
+                desc: "Un site web complet avec des aspects frontaux et dorsaux. Le site présente les services de Genie Log et comprend également une section d'administration pour gérer les clients et leurs demandes.",
+            },
+            ar: {
+                desc: "موقع ويب كامل مع جوانب الواجهة الأمامية والخلفية. يُعرض الموقع خدمات Genie Log ويشمل أيضًا قسم إداري لإدارة العملاء وطلباتهم.",
+            }
+        },
         "url": "https://genielog.000webhostapp.com/",
         "type": "web",
         "img" : "genielog.png"
     },
     {
         "name": "Tacik",
-        "desc": "Web site for Tacik.",
+        "desc": {
+            eng: {
+                desc: "Web site for Tacik.",
+            },
+            fr: {
+                desc: "Site web pour Tacik.",
+            },
+            ar: {
+                desc: "موقع ويب لـ تاجيك.",
+            },
+        },
         "url": "https://testdevelopergroup.000webhostapp.com/",
         "type": "web",
         "img" : "tacik.png"
     },
     {
         "name": "Dashboard for Management",
-        "desc": "Dashboard for Management",
+        "desc": {
+            eng: {
+                desc: "Dashboard for Management",
+            },
+            fr: {
+                desc: "Tableau de bord pour la gestion",
+            },
+            ar: {
+                desc: "لوحة التحكم للإدارة",
+            },
+        },
         "url": null,
         "type": "app-web",
         "img" : "dashbord.png"
     },
     {
         "name": "Bank Account Management Application",
-        "desc": "Some banking applications occasionally face reservations from users due to two main issues. Firstly, the design of some of these applications is criticized for its lack of visual appeal. At the same time, it is observed at certain moments that some of these applications fail to provide adequate security measures, thereby raising concerns about the protection of users' financial data.",
+        "desc": {
+            eng: {
+                desc: "Some banking applications occasionally face reservations from users due to two main issues. Firstly, the design of some of these applications is criticized for its lack of visual appeal. At the same time, it is observed at certain moments that some of these applications fail to provide adequate security measures, thereby raising concerns about the protection of users' financial data.",
+                
+            },
+            fr: {
+                desc: "Certaines applications bancaires font parfois l'objet de réserves de la part des utilisateurs en raison de deux problèmes principaux. Tout d'abord, le design de certaines de ces applications est critiqué pour son manque d'attrait visuel. En même temps, il est observé à certains moments que certaines de ces applications ne fournissent pas des mesures de sécurité adéquates, suscitant ainsi des inquiétudes quant à la protection des données financières des utilisateurs.",
+                
+            },
+            ar: {
+                desc: "تواجه بعض تطبيقات البنوك أحيانًا تحفظات من قبل المستخدمين بسبب مشكلتين رئيسيتين. أولاً، يُنتقد تصميم بعض هذه التطبيقات بسبب نقص جاذبيته البصرية. في الوقت نفسه، يُلاحظ في بعض اللحظات أن بعض هذه التطبيقات لا تقدم تدابير أمان كافية، مما يثير مخاوف بشأن حماية بيانات المستخدمين المالية.",
+                
+            },  
+        },
         "url": null,
         "type": "app-mobile",
         "img" : "bank.png"
     },
     {
         "name": "Image Processing Application",
-        "desc": "An Image Processing Application using Tkinter is a program that utilizes the Tkinter library in Python to create a graphical user interface (GUI) for performing various image processing tasks. Tkinter is a standard GUI toolkit for Python, and it allows developers to build interactive applications with ease.",
+        "desc": {
+            eng: {
+                desc: "An Image Processing Application using Tkinter is a program that utilizes the Tkinter library in Python to create a graphical user interface (GUI) for performing various image processing tasks. Tkinter is a standard GUI toolkit for Python, and it allows developers to build interactive applications with ease.",
+                
+            },
+            fr: {
+                desc: "Une application de traitement d'image utilisant Tkinter est un programme qui utilise la bibliothèque Tkinter en Python pour créer une interface utilisateur graphique (GUI) permettant d'effectuer diverses tâches de traitement d'image. Tkinter est une boîte à outils GUI standard pour Python, et elle permet aux développeurs de construire des applications interactives facilement.",
+               
+            },
+            ar: {
+                desc: "تطبيق معالجة الصور باستخدام Tkinter هو برنامج يستخدم مكتبة Tkinter في لغة Python لإنشاء واجهة مستخدم رسومية (GUI) لأداء مهام مختلفة في مجال معالجة الصور. Tkinter هي مكتبة واجهة مستخدم رسومية قياسية للغة Python، وتتيح للمطورين بناء تطبيقات تفاعلية بسهولة.",
+                
+            },
+        },
         "url": null,
         "type": "app-desktop",
         "img" : "imgapp.png"
     }
 ];
 
+
+function translate(l){
+
+
+    const elements = document.querySelectorAll('[data-translate]');
+    console.log(elements);
+    elements.forEach(element => {
+        const key = element.getAttribute('data-translate');
+        
+        let keys = key.split(".");
+
+        let al = "";
+        let translation = "";
+        if(keys.length == 1){
+            
+            let name = "", index = 0 ;
+            if(keys[0].includes('[')){
+                
+                let number = keys[0].charAt(keys[0].indexOf('[') + 1);
+                let result = keys[0].split('[')[0];
+
+                name = result ;
+                index = number;
+            }
+            else{
+
+            }
+            
+            translation = translations[l][name][index];
+
+        }
+        if(keys.length == 2 ){
+            translation = translations[l][keys[0]][keys[1]];
+        }
+        else if(keys.length == 3){
+            console.log('===');
+            let name = "", index = 0 ;
+            keys.forEach(e=>{
+                if(e.includes('[')){
+                    
+                    let number = e.charAt(e.indexOf('[') + 1);
+                    let result = e.split('[')[0];
+
+                    name = result ;
+                    index = number;
+                }
+            });
+            translation = translations[l][keys[0]][name][index][keys[2]];
+        }
+        element.textContent = translation;
+    });
+
+    if(l == 'ar'){
+        document.getElementById('logo').innerHTML = `
+        <span>م</span>حمد &nbsp; <span>ب</span>ودة
+        `;
+        document.getElementById('info_footer').innerHTML = `
+            <div class="footer-description">
+                <h3>محمد بودة</h3>
+                <p>boudamohamed060@gmail.com<p>
+            </div>
+            <small>محمد بودة ©  <span id="year"></span>, جميع الحقوق محفوضة</small>
+        `;
+        
+    }
+    else{
+        document.getElementById('logo').innerHTML = `
+            <span>B</span>ouda &nbsp; <span>M</span>ohamed
+        `; 
+        document.getElementById('info_footer').innerHTML = `
+            <div class="footer-description">
+                <h3>Bouda Mohamed</h3>
+                <p>boudamohamed060@gmail.com<p>
+            </div>
+            <small>© Bouda Mohamed  <span id="year"></span>, All rights reserved .</small>
+        `;
+        
+    }
+
+
+    
+}
+
+
+
+
+let change_lang = document.getElementById('lang'),
+mega_menu = change_lang.querySelector('.mega_menu');
+
+change_lang.addEventListener('mouseover', () => {
+    mega_menu.classList.add('hovred');
+});
+mega_menu.addEventListener('mouseout', () => {
+    mega_menu.classList.remove('hovred');
+});
+
+mega_menu.querySelectorAll('li').forEach(ele=>{
+    ele.onclick = ()=>{
+        console.log(ele.dataset.lang);
+        handleLang(ele.dataset.lang);
+        
+        
+    }
+})
+
+function handleLang(lang){
+    document.getElementById('lang-img').src = `imgs/lang/${lang}.png`;
+    if(lang == 'ar'){
+        document.documentElement.setAttribute('dir', 'rtl');
+    }
+    else{
+        document.documentElement.setAttribute('dir', 'ltr');
+    }
+    translate(lang);
+    localStorage.setItem('lang',lang);
+
+}
 
 let nav = document.querySelectorAll('.shuffle li');
 
@@ -70,7 +360,7 @@ function handleProject(name){
                 <div class="box">
                 <img src="imgs/projects/${p['img']}" alt="" />
                 <div class="caption">
-                  <h4>${p['desc']}</h4>
+                  <h4>${p['desc'][lang]['desc']}</h4>
                   <div>
                     <p>${p['name']}</p>
 
@@ -92,7 +382,7 @@ function handleProject(name){
                 <div class="box">
                 <img src="imgs/projects/${p['img']}" alt="" />
                 <div class="caption">
-                  <h4>${p['desc']}</h4>
+                  <h4>${p['desc'][lang]['desc']}</h4>
                   <div>
                     <p>${p['name']}</p>
                     <a href=""><i class="fa-regular fa-eye"></i></a>
